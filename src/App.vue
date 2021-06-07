17e41d513e34adf1a8ac8bb443e0f88105a65ab1
RivkaShabi <79815186+RivkaShabi@users.noreply.github.com>
2021-06-07 13:33:45 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  <shoppingList/>
+  </div>
+</template>
+
+<script>
+import shoppingList from '@/components/shoppingList';
+
+export default {
+  name: 'App',
+  components: {
+    shoppingList
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/shoppingList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="shopping-list">
+  <form class="pure-form" @submit.prevent="handlerSubmit">
+    <fieldset>
+        <input v-model="input" type="text" placeholder="Please enter item" />
+       
+        <button type="submit" class="pure-button pure-button-primary">submit</button>
+    </fieldset>
+</form>
+<table class="pure-table">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+          
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="(item,indexItem) of items" :key="indexItem">
+            <td>{{indexItem+1}}</td>
+            <td>{{item}}</td>
+            
+        </tr>
+      
+    </tbody>
+</table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'shoppingList',
+  data(){
+      return{
+          input:'',
+          items:[]
+      }
+  },
+  methods:{
+    handlerSubmit(){
+    this.items.push(this.input);
+    this.input='';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
